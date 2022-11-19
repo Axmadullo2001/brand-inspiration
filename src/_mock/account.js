dae52a2f0ee80af7790fae233fbe2e8e5b3b6229
axmadullo2001TATU <axmadullo2000@gmail.com>
2022-11-19 14:05:19 +0300

    Brand

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Axmadullo Ubaydullayev',
+  email: 'axmadullo2000@gmail.com',
+  photoURL: 'https://avatars.mds.yandex.net/i?id=3b80503897ede80f0a282773e4673711a2ae8421-4033955-images-thumbs&n=13',
+};
+
+export default account;
